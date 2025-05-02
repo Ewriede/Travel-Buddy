bf86a4d26295ed90711de4699711c0989b16d5cf
Ethan <ewriede@ithaca.edu>
2025-05-02 15:11:32 -0400

    combination of master adding the remove activity. May need to rework some stuff sorry.

--- app/models.py
+++ app/models.py
@@ -28,15 +28,7 @@ class User(db.Model, UserMixin):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
 
     tpacks: so.WriteOnlyMapped['ActivityToPlan'] = so.relationship('Travelplan', back_populates='owner')
-    """
-        email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                                 unique=True)
-    """
 
-    """
-    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
-        default=lambda: datetime.now(timezone.utc))
-    """
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -47,94 +39,11 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-"""
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
-    following: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        back_populates='followers')
-    followers: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.followed_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
-        back_populates='following')
-
-    def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
-
-    def follow(self, user):
-        if not self.is_following(user):
-            self.following.add(user)
-
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.following.remove(user)
-
-    def is_following(self, user):
-        query = self.following.select().where(User.id == user.id)
-        return db.session.scalar(query) is not None
-
-    def followers_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.followers.select().subquery())
-        return db.session.scalar(query)
-
-    def following_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.following.select().subquery())
-        return db.session.scalar(query)
-
-    def following_posts(self):
-        Author = so.aliased(User)
-        Follower = so.aliased(User)
-        return (
-            sa.select(Post)
-            .join(Post.author.of_type(Author))
-            .join(Author.followers.of_type(Follower), isouter=True)
-            .where(sa.or_(
-                Follower.id == self.id,
-                Author.id == self.id,
-            ))
-            .group_by(Post)
-            .order_by(Post.timestamp.desc())
-        )
-
-    def get_reset_password_token(self, expires_in=600):
-        return jwt.encode(
-            {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
-
-    @staticmethod
-    def verify_reset_password_token(token):
-        try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithms=['HS256'])['reset_password']
-        except Exception:
-            return
-        return db.session.get(User, id)
-        
-    """
-
 
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
 
-"""
-class Post(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
-
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-"""
 
 class Destination(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
